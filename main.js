1f87b9a2f5f6a84a95e3d6c42304dec5c3c7f5d9
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-10-18 16:16:26 +0700

    build: implement database connection

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
+const { Sequelize } = require('sequelize');
 
 const app = express();
+
 const port = process.env.APP_PORT || 3000;
+const host = process.env.DATABASE_HOST || 'localhost';
+const database = process.env.DATABASE_NAME;
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect: 'mssql',
+});
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -11,6 +22,14 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('DATABASE CONNECTED');
+    app.listen(port, () => {
+      console.log(`SERVER IS RUNNING ON PORT ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('UNABLE TO CONNECT TO DATABASE ', err);
+  });
